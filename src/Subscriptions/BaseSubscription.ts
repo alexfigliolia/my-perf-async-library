df69fbe746df0b086f7c610b7d3f951c06fce93d
Alex Figliolia <alexfigliolia@gmail.com>
2024-03-10 22:56:57 -0700

    Drop streams, regenerate-types

--- src/Subscriptions/BaseSubscription.ts
+++ src/Subscriptions/BaseSubscription.ts
@@ -1,14 +1,10 @@
-import type { GQLSubscription } from "@alexfigliolia/graphql-client";
-import { JobStatus } from "GQL/Types";
 import type { IOptions, Pull } from "Pull";
-import type { Activity, IBaseSubscription } from "./types";
+import type { IBaseSubscription } from "./types";
 
 export abstract class BaseSubscription<P extends IOptions, T extends Pull<P>>
   implements IBaseSubscription<P, T>
 {
   public closed = false;
-  public activity: Activity = "poll";
-  abstract stream: GQLSubscription<any, any>;
 
   abstract initialize(): BaseSubscription<P, T>;
 
@@ -17,27 +13,14 @@ export abstract class BaseSubscription<P extends IOptions, T extends Pull<P>>
   abstract createPull(job: P): T;
 
   public destroy() {
-    this.stream.closeAll();
     this.closed = true;
   }
 
   public onPoll(job?: P | null) {
-    if (!job) {
-      this.activity = "stream";
-    } else {
-      this.activity = "poll";
+    if (job) {
       const pull = this.createPull(job);
       void this.enqueue(pull);
     }
-  }
-
-  public onStream(job?: P) {
-    if (this.activity !== "stream" || !job) {
-      return;
-    }
-    const pull = this.createPull(job);
-    void pull.setJobStatus(job.id, JobStatus.Inprogress);
-    void this.enqueue(pull);
   }
 
   public async enqueue(pull: T) {
--- src/Subscriptions/types.ts
+++ src/Subscriptions/types.ts
@@ -1,19 +1,13 @@
-import type { GQLSubscription } from "@alexfigliolia/graphql-client";
 import type { IOptions, Pull } from "Pull";
 import type { BaseSubscription } from "./BaseSubscription";
 
-export type Activity = "stream" | "poll";
-
 export interface IBaseSubscription<P extends IOptions, T extends Pull<P>> {
   closed: boolean;
-  activity: Activity;
-  stream: GQLSubscription<any, any>;
   initialize: () => BaseSubscription<P, T>;
   poll: () => Promise<void>;
   createPull: (job: P) => T;
   destroy: () => void;
   onPoll: (job?: P | null) => void;
-  onStream: (job?: P) => void;
   enqueue: (pull: T) => Promise<void>;
 }
 
